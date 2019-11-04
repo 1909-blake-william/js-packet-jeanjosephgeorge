970f192c0c6d9978d0eca17bcb4d3aa9766a4182
jeanjosephgeorge <jean.joseph@outlook.com>
2019-11-04 15:06:48 -0500

    completed two

--- packet.js
+++ packet.js
@@ -1,6 +1,6 @@
 // Javascript Homework
 // Due next Friday Morning
-// 
+//
 // Jean Joseph George
 // -----------------------------------------------------------------------------------
 // PART I
@@ -13,59 +13,61 @@
 // Define function: fib(n)
 // Return the nth number in the fibonacci sequence.
 
-function fib(n){
-    if (n < 2) {
-        return 1;
-    } else {
-        return fib(n - 2) + fib(n - 1);
-    }
+function fib(n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    return fib(n - 2) + fib(n - 1);
+  }
 }
 //console.log(fib(7));
-
 
 // 2. Bubble Sort
 // Define function: bubbleSort(numArray)
 // Use the bubble sort algorithm to sort the array.
 // Return the sorted array.
 
-function bubbleSort(numArray){
-    let arr = numArray;
+function bubbleSort(numArray) {
+let arr = numArray;
+let loop = true;
 
-    for(let i=0;i<arr.length;i++){
-
+for (let x = 0; x < arr.length; x++) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+        let temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+        } else {
+        continue;
+        }
+        }
     }
-}
+return arr;
+} 
 
+console.log(bubbleSort([7, 2, 1, 23, 7, 3, 2, 6]));
 
 // 3. Reverse String
 // Define function: reverseStr(someStr)
 // Reverse and return the String.
 
-
-
 // 4. Factorial
 // Define function: factorial(someNum)
 // Use recursion to compute and return the factorial of someNum.
-
-
 
 // 5. Substring
 // Define function substring(someStr, length, offset)
 // Return the substring contained between offset and (offset + length) inclusively.
 // If incorrect input is entered, use the alert function and describe why the input was incorrect.
 
-
-
 // 6. Even Number
 // Define function: isEven(someNum)
 // Return true if even, false if odd.
 // Do not use % operator.
 
-
 // 7. Palindrome
 // Define function isPalindrome(someStr)
 // Return true if someStr is a palindrome, otherwise return false
-
 
 // 8. Shapes
 // Define function: printShape(shape, height, character)
@@ -89,11 +91,9 @@ function bubbleSort(numArray){
 //  ***
 //   *
 
-
 // 9. Object literal
 // Define function traverseObject(someObj)
 // Print every property and it's value.
-
 
 // 10. Delete Element
 // Define function deleteElement(someArr)
@@ -102,7 +102,6 @@ function bubbleSort(numArray){
 // Print length
 // The lengths should be the same.
 
-
 // 11. Splice Element
 // Define function spliceElement(someArr)
 // Print length
@@ -110,15 +109,12 @@ function bubbleSort(numArray){
 // Print length
 // The lengths should be one less than the original length.
 
-
 // 12. Defining an object using a constructor
 // Define a function Person(name, age)
 // The following line should set a Person object to the variable john:
 // 	let john = new Person("John", 30);
 
-
 // 13. Defining an object using an object literal
 // Define function getPerson(name, age)
 // The following line should set a Person object to the variable john:
 // 	let john = getPerson("John", 30);
-
